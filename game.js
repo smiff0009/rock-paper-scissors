918d04bd0458d25199324396b23933321aa4f39e
smiff0009 <trevorgustaveson@gmail.com>
2025-01-02 12:08:19 -0700

    test

--- game.js
+++ game.js
@@ -30,3 +30,56 @@ function getHumanChoice() {
 console.log("---");
 getHumanChoice();
 console.log(`Your choice is ${humanChoice.toLowerCase()}`);
+console.log("---");
+
+function playRound(humanChoice, computerChoice) {
+    switch(humanChoice) {
+        case "rock": {
+            if(computerChoice == "paper") {
+                console.log("Computer wins! Paper beats rock!");
+                computerScore++;
+            }
+            else if(computerChoice == "scissors") {
+                console.log("You win! Rock beats scissors!");
+                humanScore++;
+            }
+            else {
+                console.log("It's a tie!");
+            }
+        }
+        break;
+        case "paper": {
+            if(computerChoice == "rock") {
+                console.log("You win! Paper beats rock!");
+                humanScore++;
+            }
+            else if(computerChoice == "scissors") {
+                console.log("Computer wins! Scissors beats paper!");
+                computerScore++;
+            }
+            else {
+                console.log("It's a tie!");
+            }
+        }
+        break;
+        case "scissors": {
+            if(computerChoice == "rock") {
+                console.log("Computer wins! Rock beats scissors!");
+                computerScore++;
+            }
+            else if(computerChoice == "paper") {
+                console.log("You win! Scissors beats paper!");
+                humanScore++;
+            }
+            else {
+                console.log("It's a tie!");
+            }
+        }
+        break;
+    }
+}
+
+playRound(humanChoice, computerChoice);
+
+console.log(humanScore);
+console.log(computerScore);
