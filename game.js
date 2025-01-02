5b5a1693f11d6edc8cd9f61a7228ed547b334861
smiff0009 <trevorgustaveson@gmail.com>
2025-01-02 10:40:07 -0700

    test

--- game.js
+++ game.js
@@ -8,4 +8,20 @@ function getComputerChoice() {
     return computerChoice;
 } 
 
+console.log(options[0]);
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let humanInput = prompt("Choose rock, paper, or scissors");
+    if(humanInput.toLowerCase() === "rock" || "paper" || "scissors") {
+        let humanChoice = humanInput.toLowerCase();
+        return humanChoice;
+    }
+    else {
+        alert("not a valid choice!")
+    }
+}
+
+console.log("---");
+
+console.log(`Your choice is ${humanChoice}`);
