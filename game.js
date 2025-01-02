1aa938f0652792aa81bbbb39371d4010fe65debc
smiff0009 <trevorgustaveson@gmail.com>
2025-01-02 10:31:35 -0700

    test

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-console.log("test");
+let humanScore = 0;
+let computerScore = 0;
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    let computerChoice = options[Math.floor(Math.random * options.length)];
+    return computerChoice;
+} 
+
+console.log(getComputerChoice());
